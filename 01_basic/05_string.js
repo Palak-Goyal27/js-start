45c1f0c7736bdd0623ef4280ad4ba672ef5491a9
Palak-Goyal27 <palakgoyal2727@gmail.com>
2025-02-28 09:55:57 +0000

    string

--- 01_basic/05_string.js
+++ 01_basic/05_string.js
@@ -1,1 +1,10 @@
-console.log("hello")
+console.log("hello");
+const name = "Palak";
+const age = 22;
+
+console.log(name+age+"mewar"); // this is old version
+
+// new version is
+
+console.log(`hello my name is ${name} and my age is ${age}`);
+
